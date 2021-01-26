c0516c47f9bcb16115223e073df4d43267fd1239
Kerem Sevencan <kerem@ritmix.org>
2021-01-26 16:44:24 +0100

    adding lowercase first

--- src/utils/capitalize.js
+++ src/utils/capitalize.js
@@ -1,5 +1,6 @@
 export const capitalizeSnakeCase = snakeCaseWord =>
   snakeCaseWord
+    .toLowerCase()
     .split('_')
     .map(word => word.charAt(0).toUpperCase() + word.slice(1))
     .join(' ')
